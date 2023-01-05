70262673648eabcaf70c1bbf6040d2006b7f79bf
Boy <sivertpaulsen2@gmail.com>
2023-01-05 15:46:59 +0100

    template

---
+++ src/main/kotlin/com/boy0000/template/TemplatePlugin.kt
@@ -0,0 +1,13 @@
+package com.boy0000.template
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TemplatePlugin : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
